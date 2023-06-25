d06e06ddc66e896d6dc291b19f57272957829d38
ajinnarapu1986 <ajinnarapu1986@gmail.com>
2023-06-25 21:09:31 +0530

    modified log4j2.xml

--- src/main/java/flink/ClickHouseConsumer.java
+++ src/main/java/flink/ClickHouseConsumer.java
@@ -1,10 +1,6 @@
 package flink;
 
-import java.util.HashMap;
-
 import org.apache.flink.api.common.functions.MapFunction;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,16 +13,11 @@ public class ClickHouseConsumer implements MapFunction<String, String> {
 	 */
 	private static final long serialVersionUID = 868789200034301194L;
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public String map(String value) throws Exception {
 		
 		
 		log.info("Pushing to ClickHouse : " + value);
-		
-		ObjectMapper mapper = new ObjectMapper();
-		HashMap<String, Object> mapData = mapper.readValue(value, HashMap.class);
-		System.out.println(mapData.keySet());
 		
 		//#############################Processing Logic
 		
